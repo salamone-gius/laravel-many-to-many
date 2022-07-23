f2d677293f7a0fd6957ea161d70672a593bdbbf6
salamone-gius <salamone.gius@gmail.com>
2022-07-23 14:15:56 +0200

    'tags' table created

--- database/migrations/2022_07_23_121109_create_tags_table.php
+++ database/migrations/2022_07_23_121109_create_tags_table.php
@@ -15,6 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('slug', 150)->unique();
             $table->timestamps();
         });
     }
